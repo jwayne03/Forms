b86a5ca64243badf4bc21fdb35735cdef653a277
John Wayne Carreon <55916874+jwayne03@users.noreply.github.com>
2020-11-21 15:43:18 +0100

    #4 I've forgot to add the files

---
+++ src/app/credits/credits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: "./credits.component.html",
+    styleUrls: ["./credits.component.css"]
+})
+
+export class CreditsComponent {
+
+}
